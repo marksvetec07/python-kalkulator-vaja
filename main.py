1d5a93e3d70ea0ce03972d87bf84be786f5be7f5
Mark Svetec <mark.svetec1@gmail.com>
2026-03-16 23:02:30 +0100

    Dodana preprosta zgodovina zadnjih 3 izračunov

--- main.py
+++ main.py
@@ -12,36 +12,49 @@ def deli(a, b):
         return "Napaka: deljenje z 0 ni dovoljeno!"
     return a / b
 
-print("Pozdrav! Mini kalkulator")
-print("0 = izhod")
-print("1 = seštevanje")
-print("2 = odštevanje")
-print("3 = množenje")
-print("4 = deljenje")
+zgodovina = []
 
-izbira = input("Kaj želiš narediti? (0, 1, 2, 3 ali 4): ")
+while True:
+    print("\nPozdrav! Mini kalkulator")
+    print("0 = izhod")
+    print("1 = seštevanje")
+    print("2 = odštevanje")
+    print("3 = množenje")
+    print("4 = deljenje")
 
-if izbira == "0":
-    print("Program se zapira. Nasvidenje!")
-elif izbira == "1":
-    x = float(input("Prvo število: "))
-    y = float(input("Drugo število: "))
-    rezultat = sestej(x, y)
-    print(f"Rezultat seštevanja števil {x} in {y} je {rezultat}.")
-elif izbira == "2":
-    x = float(input("Prvo število: "))
-    y = float(input("Drugo število: "))
-    rezultat = odstej(x, y)
-    print(f"Rezultat odštevanja števil {x} in {y} je {rezultat}.")
-elif izbira == "3":
-    x = float(input("Prvo število: "))
-    y = float(input("Drugo število: "))
-    rezultat = pomnozi(x, y)
-    print(f"Rezultat množenja števil {x} in {y} je {rezultat}.")
-elif izbira == "4":
-    x = float(input("Prvo število: "))
-    y = float(input("Drugo število: "))
-    rezultat = deli(x, y)
-    print(f"Rezultat deljenja števil {x} in {y} je {rezultat}.")
-else:
-    print("Neveljavna izbira!")
+    izbira = input("Kaj želiš narediti? (0, 1, 2, 3 ali 4): ")
+
+    if izbira == "0":
+        print("Program se zapira. Nasvidenje!")
+        break
+
+    elif izbira in ["1", "2", "3", "4"]:
+        x = float(input("Prvo število: "))
+        y = float(input("Drugo število: "))
+
+        if izbira == "1":
+            rezultat = sestej(x, y)
+            zapis = f"Seštevanje: {x} + {y} = {rezultat}"
+        elif izbira == "2":
+            rezultat = odstej(x, y)
+            zapis = f"Odštevanje: {x} - {y} = {rezultat}"
+        elif izbira == "3":
+            rezultat = pomnozi(x, y)
+            zapis = f"Množenje: {x} * {y} = {rezultat}"
+        elif izbira == "4":
+            rezultat = deli(x, y)
+            zapis = f"Deljenje: {x} / {y} = {rezultat}"
+
+        print(zapis)
+
+        zgodovina.append(zapis)
+
+        if len(zgodovina) > 3:
+            zgodovina.pop(0)
+
+        print("\nZadnji 3 izračuni:")
+        for element in zgodovina:
+            print(element)
+
+    else:
+        print("Neveljavna izbira!")
